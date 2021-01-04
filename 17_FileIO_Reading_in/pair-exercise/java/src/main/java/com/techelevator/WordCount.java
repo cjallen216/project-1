154c7a55c6c51f79caa6d59599208e5e5860b0d3
Mehvish Baig <mehvishbaig@yahoo.com>
2021-01-04 17:34:01 -0500

    Latest file 5:33

--- 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
+++ 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
@@ -8,7 +8,52 @@ import java.util.Scanner;
 public class WordCount {
 
 	public static void main(String[] args) {
+		wordCount();
 
+	}
+	
+	public static void wordCount()
+	{
+		String filePath = "alices_adventures_in_wonderland.txt";
+		File theFile = new File(filePath);
+//		
+//		System.out.println(theFile.toString());
+//		System.out.println(theFile.getAbsolutePath());
+//		System.out.println(theFile.exists());
+//		System.out.println("Directory: " + theFile.isDirectory());
+//		System.out.println("File: " + theFile.isFile());
+		
+		Scanner scanner;
+		
+		try 
+		{
+			scanner = new Scanner(theFile);
+			while(scanner.hasNextLine())
+			{
+				//Reading each line on the file
+				String eachLine = scanner.nextLine();
+				
+				//Splitting the words by spaces
+				String[] words = eachLine.split(" ");
+				//return words.length;
+				
+				
+				int totalWords = 0;
+				for(int i = 1; i < words.length - 1; i++)
+				{
+					
+					//words.length++;
+					
+				}
+				System.out.println(totalWords);
+			}
+			
+		}
+		catch (Exception e)
+		{
+			// TODO: handle exception
+		}
+		
 	}
 
 }
