aacf80ef923ee6d8fd3f0e9a3d531cd147c8c603
Chris Allen <christopherjallen216@gmail.com>
2021-01-04 21:02:45 -0500

    I think I got the answer, check out my notes in the code

--- 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
+++ 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
@@ -7,22 +7,25 @@ import java.util.Scanner;
 
 public class WordCount {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		wordCount();
-
 	}
 	
 	public static void wordCount()
 	{
 		String filePath = "alices_adventures_in_wonderland.txt";
 		File theFile = new File(filePath);
-//		
+		
 //		System.out.println(theFile.toString());
 //		System.out.println(theFile.getAbsolutePath());
 //		System.out.println(theFile.exists());
 //		System.out.println("Directory: " + theFile.isDirectory());
 //		System.out.println("File: " + theFile.isFile());
 		
+		int totalWords = 0;
+		int totalSentences = 0;
+				
 		Scanner scanner;
 		
 		try 
@@ -33,27 +36,33 @@ public class WordCount {
 				//Reading each line on the file
 				String eachLine = scanner.nextLine();
 				
-				//Splitting the words by spaces
-				String[] words = eachLine.split(" ");
-				//return words.length;
-				
-				
-				int totalWords = 0;
-				for(int i = 1; i < words.length - 1; i++)
+				// if each line is NOT empty
+				if (!eachLine.isEmpty())
 				{
-					
-					//words.length++;
-					
+					//Splitting the words by spaces - "\\s+" is code for white spaces 
+					String[] words = eachLine.split("\\s+");
+					totalWords += words.length;
+				}	
+				if (eachLine.contains(".") || eachLine.contains("?") || eachLine.contains("!"))
+				{
+					// add a new sentence to the total
+					totalSentences++;
 				}
-				System.out.println(totalWords);
+				
+				//int totalWords = 0; <-- moved before the try-catch
+				//for(int i = 1; i < words.length - 1; i++) <-- not needed
 			}
 			
 		}
 		catch (Exception e)
 		{
 			// TODO: handle exception
+			System.out.println("Your file does not exist");
 		}
 		
+		// print out the answers after the try-catch
+		System.out.println("Word Count: " + totalWords);
+		System.out.println("Sentence Count: " + totalSentences);
 	}
 
 }
